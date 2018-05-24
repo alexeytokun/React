522b2c4845b22a935a3be61e1ae5fa3c4d5ed381
alexeytokun <alexeytokun@gmail.com>
2018-05-24 11:32:07 +0300

    change token expiration time to 365 days

--- project/backend/modules/routes/signin.js
+++ project/backend/modules/routes/signin.js
@@ -28,7 +28,7 @@ router.post('/', (req, res, next) => {
     login(req.body)
         .then(userData => {
             delete userData.password;
-            let token = jwt.sign({ role: userData.role }, jwtKey, { expiresIn: 60 * 60 });
+            let token = jwt.sign({ role: userData.role }, jwtKey, { expiresIn: '365d' });
             return { token: token, userdata: userData };
         })
         .catch(result => {
